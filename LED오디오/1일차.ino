6f4c73678f36a44e293788c287ed78903014036e
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2022-01-05 23:20:02 +0900

    LED오디오 1일차

---
+++ LED오디오/1일차.ino
@@ -0,0 +1,53 @@
+#include <HuemonelabKit.h>
+
+Led led1(2);
+Led led2(3);
+Led led3(4);
+Led led4(5);
+Led led5(6);
+Led led6(7);
+Led led7(8);
+Led led8(9);
+Led led9(10);
+Led led10(11);
+
+void setup() {
+  
+}
+
+void loop() {
+  /*
+  led1.on();
+  delay(200);
+  led1.off();
+  led2.on();
+  delay(200);
+  led2.off();
+  led3.on();
+  delay(200);
+  led3.off();
+  led4.on();
+  delay(200);
+  led4.off();
+  led5.on();
+  delay(200);
+  led5.off();
+  led6.on();
+  delay(200);
+  led6.off();
+  led7.on();
+  delay(200);
+  led7.off();
+  led8.on();
+  delay(200);
+  led8.off();
+  led9.on();
+  delay(200);
+  led9.off();
+  led10.on();
+  delay(200);
+  led10.off();
+  */
+  delay(100);
+
+}
